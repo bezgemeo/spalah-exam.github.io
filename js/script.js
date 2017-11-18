78997fd807af0a4569d4d646f15f9fbf79842ae6
tamara.s <tamara.strukova@gmail.com>
2017-11-18 15:20:47 +0200

    added smooth scroll for navigation links

--- js/script.js
+++ js/script.js
@@ -70,4 +70,21 @@ $(document).ready(function() {
         });
     }
 
+    $('.smooth-scroll').on('click','a', function(event) {
+        //отменяем стандартную обработку нажатия по ссылке
+        event.preventDefault();
+
+        //забираем идентификатор блока с атрибута href
+        var id = $(this).attr('href'),
+
+            //узнаем высоту от начала страницы до блока на который ссылается якорь
+        top = $(id).offset().top;
+
+        //анимируем переход на расстояние - top за 700 мс
+        $('body,html').animate({scrollTop: top}, 700);
+    });
+
+
+
+
 });
